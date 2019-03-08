9fc6ac4d0fa199ddb5f5fffd44d25893c597e31a
Viviane <viviane.a.gaspar@gmail.com>
2019-03-08 09:09:20 -0300

    Continuidade no js para dar funcionalidade de filtrar e ordenar

--- src/main.js
+++ src/main.js
@@ -1,26 +1,78 @@
-const LOL_ITEMS = LOL.data;
-// console.log(LOL_ITEMS);
+const LOL_CARD = LOL.data;
+console.log(LOL_CARD);
 
-const renderHTML = (e) => {
-  const dropFilter = e.target.value;
-  console.log(dropFilter);
+const LOL_VALUES = Object.values(LOL_CARD);
+console.log(LOL_VALUES);
+
+let dropFilter = "";
+let dropOrder = "";
+
+const lolTemplate = card => {
+  return `<div class="card">
+  <h3 class="card-title">${card.name}</h3>
+  <img src="${card.splash}" alt="Imagem do personagem ${card.name}" class="card-img">
+  <ul class="card-list">
+    <li class="card-item">Ataque: ${card.info.attack}</li>
+    <li class="card-item">Defesa: ${card.info.defense}</li>
+    <li class="card-item">Magia: ${card.info.magic}</li>
+    <li class="card-item">Dificuldade: ${card.info.difficulty}</li>
+  </ul>
+  </div>`;
+};
+
+
+
+
+// lista todos os cards sem nenhum filtro - begin
+const LOL_CARDS = LOL_VALUES.map(card => {
+  return lolTemplate(card);
+});
+console.log(LOL_CARDS.length);
+
+document.querySelector(".card-content").innerHTML = LOL_CARDS.join("");
+// lista todos os cards sem nenhum filtro - end
+
+
+
+
+// adiciona o evento ao Filtrar por Classe - begin
+document.querySelector(".drop-filter").addEventListener("change", event => {
+  dropFilter = event.target.value;
   
-  const LOL_CARDS = Object.values(LOL_ITEMS).filter(card => card.tags.includes(dropFilter) || !dropFilter).map(card => {
-    return `<div class="card">
-    <h3 class="card-title">${card.name}</h3>
-    <img src="${card.splash}" alt="Imagem do personagem ${card.name}" class="card-img">
-    </div>`;
+  const LOL_CARDS = LOL_VALUES.filter(card => {
+    return card.tags.indexOf(dropFilter) >= 0;
+  }).map(card => {
+    return lolTemplate(card);
   });
-  
-  // console.log(LOL_CARDS);
-  
-  if (dropFilter !== undefined) {
-    document.querySelector(".card-subtitle").innerHTML = `Foram encontrados ${LOL_CARDS.length} itens de ${dropFilter}`;
-  }
+  console.log(LOL_CARDS.length);
   
   document.querySelector(".card-content").innerHTML = LOL_CARDS.join("");
-}
+});
+// adiciona o evento ao Filtrar por Classe - end
 
-window.addEventListener("load", renderHTML);
 
-document.querySelector(".drop-filter").addEventListener("change", renderHTML);
+
+
+// adiciona o evento ao Ordenar por - begin
+document.querySelector(".drop-order").addEventListener("change", event => {
+  dropOrder = event.target.value;
+  
+  const LOL_CARDS = LOL_VALUES.sort((cardA, cardB) => {
+    return cardB.info[dropOrder] - cardA.info[dropOrder];
+  }).map(card => {
+    return lolTemplate(card);
+  });
+  console.log(LOL_CARDS.length);
+  
+  document.querySelector(".card-content").innerHTML = LOL_CARDS.join("");
+});
+// adiciona o evento ao Ordenar por - end
+
+
+
+
+document.querySelectorAll(".card-img").forEach(cardImg => {
+  cardImg.addEventListener("click", event => {
+    console.log(event);
+  });
+});
